27e55e50efd8afdd418e765d5b777002fb8f7447
Colin Frederickson <110066636+colinfrederickson@users.noreply.github.com>
2025-05-18 10:32:43 -0500

    Use API_BASE_URL env variable

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,9 @@
 // src/utils/api.js
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+
 export const loginUser = async (data, rememberMe) => {
   try {
-    const response = await fetch('http://localhost:8000/api/login', {
+    const response = await fetch(`${API_BASE_URL}/login`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data),
